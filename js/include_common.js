3b9964adfb114b2bcb8740c0a2a3d60acd4f9674
ashwin.khandare@gmail.com <The_Monk@Ashwins-MacBook-Pro.local>
2016-11-07 21:38:28 -0500

    all files in courses

--- js/include_common.js
+++ js/include_common.js
@@ -1,6 +1,6 @@
 
 			$(document).ready(function(){
-			$("#header").load("/Users/devika_gues1/Documents/dhamma_ui/html/header.html");
-			$("#navbar").load("/Users/devika_gues1/Documents/dhamma_ui/html/navbar.html");
-        		$("#footer").load("/Users/devika_gues1/Documents/dhamma_ui/html/footer.html");
+			$("#header").load("../../html/header.html");
+			$("#navbar").load("../..//html/navbar.html");
+        		$("#footer").load("../../html/footer.html");
 			});
