38c6117bf0f8072af5958819d5759ddc9d591a32
dhammaDelaware <patil.devika@gmail.com>
2017-03-08 11:30:42 -0500

    Tracking all the changes since december 2016

--- js/absolute_url.js
+++ js/absolute_url.js
@@ -1,2 +1,2 @@
-var ABSOLUTE_URL = 'http://www.midatlantic.us.dhamma.org/ne/dhamma_ui';
-var ABSOLUTE_URL_TEST = 'http://localhost/~devika_gues1/dhamma_ui';
+var ABSOLUTE_URL = 'http://www.delaware.dhamma.org/ne/dhamma_ui';
+var ABSOLUTE_URL_TEST = 'http://localhost/~dhamma1/dhamma_ui';
