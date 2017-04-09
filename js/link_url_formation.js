bffb2775ae017af5a6774a31249c7821763dd1e1
dhammaDelaware <patil.devika@gmail.com>
2017-04-09 04:51:37 -0400

    added child pages for old student sub menu items

--- js/link_url_formation.js
+++ js/link_url_formation.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+	$('.img-path').attr('src', ABSOLUTE_URL + '/image/MAVA_regions.png');
 	$(document).on('click','.link-navbar',function(){
 		var a_href = $(this).attr('href');
+		
 		$('.link-navbar').attr('href',ABSOLUTE_URL + a_href);
 	}); 
+	
 })
 
