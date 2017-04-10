9485ee569b5798e5514d0991eb233cf9073560f1
ashwin.khandare@gmail.com <The_Monk@Ashwins-MacBook-Pro.local>
2017-04-10 15:15:15 -0400

    Updated donations

--- js/absolute_url.js
+++ js/absolute_url.js
@@ -1,3 +1,3 @@
 var ABSOLUTE_URL_MAVA = 'http://www.midatlantic.us.dhamma.org/ne/dhamma_ui';
-var ABSOLUTE_URL_TEST = 'http://localhost/~dhamma1/dhamma_ui';
-var ABSOLUTE_URL = 'http://www.delaware.dhamma.org/ne/dhamma_ui';
+var _ABSOLUTE_URL = 'http://www.delaware.dhamma.org/ne/dhamma_ui';
+var ABSOLUTE_URL = 'http://localhost/~The_Monk/dd/dhamma_ui';
