facc927bb1ee091110ae876bf17d403141a7068a
dhammaDelaware <patil.devika@gmail.com>
2017-04-18 23:10:12 -0400

    fixed the map for MAVA region

--- js/link_url_formation.js
+++ js/link_url_formation.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.img-path').attr('src', ABSOLUTE_URL + '/image/MAVA_regions.png');
+	$('.img-path').attr('src', ABSOLUTE_URL + '/image/MAVA_East_Regions.gif');
 	$(document).on('click','.link-navbar',function(){
 		var a_href = $(this).attr('href');
 		alert(a_href);
