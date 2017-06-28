de87bd025cad4b6553c30d9854578cc15f3aeaf5
dhammaDelaware <patil.devika@gmail.com>
2017-06-27 21:39:40 -0400

    defect fixes

--- js/link_url_formation.js
+++ js/link_url_formation.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {	
 	$(document).on('click','.link-navbar',function(){
-		location.reload();
+		alert("inside linkurl formation");
+		alert("before forming a_href" + a_href);
 		var a_href = $(this).attr('href');
-		$('.link-navbar').attr('href',ABSOLUTE_URL + a_href);	
+		alert("after forming a_href" + a_href);
+		$(this).attr('href',ABSOLUTE_URL + a_href);
 	}); 
 	
 })
