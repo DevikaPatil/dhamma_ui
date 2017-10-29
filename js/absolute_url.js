9ef5bd4886c0b04156fc3e59eca4d7f67ff419c1
ashwin.khandare@gmail.com <The_Monk@Ashwins-MacBook-Pro.local>
2017-10-29 19:30:07 -0400

    fixed center snow pic

--- js/absolute_url.js
+++ js/absolute_url.js
@@ -1,4 +1,4 @@
 var ABSOLUTE_URL_MAVA = 'http://www.midatlantic.us.dhamma.org/ne/dhamma_ui';
-var ABSOLUTE_URL = 'http://www.delaware.dhamma.org/ne/dhamma_ui';
-var ABSOLUTE_URL_TEST2 = 'http://localhost/~The_Monk/dd/dhamma_ui';
+var ABSOLUTE_URL_TEST = 'http://www.delaware.dhamma.org/ne/dhamma_ui';
+var ABSOLUTE_URL = 'http://localhost/~The_Monk/dd/dhamma_ui';
 var ABSOLUTE_URL_TEST1 = 'http://localhost/~dhamma1/dhamma_ui';
